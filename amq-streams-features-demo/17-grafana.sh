d53e3e7182687bc589dacdcf0a845eb83753e3a5
Stelios Kousouris <stkousso@stkousso.fedora>
2019-01-30 14:27:58 +0000

    adding prometheus metrics for zookeeper and kafka

--- amq-streams-features-demo/17-grafana.sh
+++ amq-streams-features-demo/17-grafana.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # create Prometheus datasource
-curl -X POST http://admin:admin@grafana-route-myproject.127.0.0.1.nip.io/api/datasources -d @grafana-dashboards/datasource.json --header "Content-Type: application/json"
+#curl -X POST http://admin:admin123@grafana-route-amq-streams-demo.192.168.42.162.nip.io/api/datasources -d @grafana-dashboards/datasource.json --header "Content-Type: application/json"
 # deploy Kafka dashboard
-curl -X POST http://admin:admin@grafana-route-myproject.127.0.0.1.nip.io/api/dashboards/db -d @grafana-dashboards/strimzi-kafka.json --header "Content-Type: application/json"
+curl -X POST http://admin:admin123@grafana-route-amq-streams-demo.192.168.42.162.nip.io/api/dashboards/db -d @grafana-dashboards/strimzi-kafka.json --header "Content-Type: application/json"
 # deploy Zookeeper dashboard
-curl -X POST http://admin:admin@grafana-route-myproject.127.0.0.1.nip.io/api/dashboards/db -d @grafana-dashboards/strimzi-zookeeper.json --header "Content-Type: application/json"
+curl -X POST http://admin:admin123@grafana-route-amq-streams-demo.192.168.42.162.nip.io/api/dashboards/db -d @grafana-dashboards/strimzi-zookeeper.json --header "Content-Type: application/json"
